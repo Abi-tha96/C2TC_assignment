c50984ac2aba088e8c62a06194eb60b3296720df
Sri <Sri@LAPTOP-C4QF4B05>
2025-07-21 20:50:56 +0530

    assingnment 2

---
+++ C2TC_Assignment/src/com/abitha/assignment2/SalesMain.java
@@ -0,0 +1,10 @@
+package com.abitha.assignment2;
+
+public class SalesMain {
+	 public static void main(String[] args) {
+	        Commission c1 = new Commission();
+	        c1.acceptDetails();
+	        c1.calculateCommission();
+	    }
+	}
+
